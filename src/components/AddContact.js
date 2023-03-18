80ed11dceb5ae9df58721279ffeb61edd22e3964
Patienceh1 <patiencehodzi@gmail.com>
2023-03-18 17:29:05 +0000

    AddContact

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -12,15 +12,44 @@ class AddContact extends React.Component {
             alert("All fields are mandetory");
             return;
         }
-    this.props.AddContactHandler(this.state);
-    this.setState({name:"", email:"", place:""});
-}
+        this.props.AddContactHandler(this.state);
+        this.setState({ name: "", email: "", place: "" });
+    }
 
-render() {
-    return (
-        <div className="main">
-            <h1>Add AddContact</h1>
-        </div>
-    )
+    render() {
+        return (
+            <div className="main">
+                <h2>Add AddContact</h2>
+                <form className="form" onSubmit={this.add}></form>
+                <div className="field">
+                    <label>Name</label>
+                    <input type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={this.state.name}
+                        onChange={(e) => this.setState({ name: e.target.value })}></input>
+                </div>
+                <div className="field">
+                    <label>Email</label>
+                    <input
+                    type="email"
+                    email="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={(e) => this.setState({  email: e.target.value })}></input>
+                </div>
+                <div className="field">
+                    <label></label>
+                    <input
+                    type="text"
+                    place="place"
+                    placeholder="Place"
+                    value={this.state.place}
+                    onChange={(e) => this.setState({ place: e.target.value })}></input>
+                </div>
+                <button className="button">Add</button>
+            </div>
+        )
+    }
 }
-}
+export default AddContact;
